672c30cedabfe1b210e90ca79f3a9fa058a79619
KORAY-AMAN-ASLAN <aman.arabzadeh98@gmail.com>
2024-09-21 10:24:08 +0200

    Usefull Lib

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <gmp.h> // Gnu MultiPrecision Library.
+#include <iostream>
+
+
+// To compile 
+// Read more: // https://home.cs.colorado.edu/~srirams/courses/csci2824-spr14/gmpTutorial.html
+// g++ -o factorial main.cpp -lgmp
+//./factorial
+
+int main() {
+    // Init
+    mpz_t result;
+    mpz_init(result);
+    
+    // fact 50!
+    mpz_fac_ui(result, 50);
+
+
+    std::cout << "1000! = ";
+    // Base 10
+    mpz_out_str(stdout, 10, result);
+    std::cout << std::endl;
+
+    // Clear memory
+    mpz_clear(result);
+
+    return 0;
+}
